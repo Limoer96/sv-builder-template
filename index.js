994183507e7cb0ab7b8d551a79133102dd3d2e5a
Limoer96 <myt19960503@outlook.com>
2020-07-20 16:33:43 +0800

    [!]修复prettier格式化ts错误

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const fs = require("fs");
-const path = require("path");
-const mkdirp = require("mkdirp");
-const prettier = require("prettier");
+const fs = require('fs')
+const path = require('path')
+const mkdirp = require('mkdirp')
+const prettier = require('prettier')
 
-const fsp = fs.promises;
+const fsp = fs.promises
 function isEmpty(value) {
-  return value == undefined || value === "";
+  return value == undefined || value === ''
 }
 /**
  * 写入文件
@@ -14,87 +14,87 @@ function isEmpty(value) {
  */
 function writeFile(fileName, content) {
   if (!fileName || isEmpty(content)) {
-    return;
+    return
   }
   fsp.writeFile(fileName, content).catch((err) => {
-    console.error("写入文件错误", err.message);
-  });
+    console.error('写入文件错误', err.message)
+  })
 }
 
 const TypeMap = {
-  integer: "number",
-  string: "string",
-  number: "number",
-};
+  integer: 'number',
+  string: 'string',
+  number: 'number',
+}
 
 class Template {
   constructor(docs) {
-    this.docs = docs;
-    const config = global.SERVICE_CONFIG || {};
-    this.rootDir = path.join(process.cwd(), config.outDir);
-    this.ext = config.ext; // 扩展名
-    this.currentDoc = docs[0];
+    this.docs = docs
+    const config = global.SERVICE_CONFIG || {}
+    this.rootDir = path.join(process.cwd(), config.outDir)
+    this.ext = config.ext // 扩展名
+    this.currentDoc = docs[0]
   }
   renderContent() {
-    const multiMethod = this.docs && this.docs.length > 1;
+    const multiMethod = this.docs && this.docs.length > 1
     for (let doc of this.docs) {
-      this.currentDoc = doc;
-      const { fileName, dirName, method } = doc;
-      const relativePath = path.join(this.rootDir, dirName);
+      this.currentDoc = doc
+      const { fileName, dirName, method } = doc
+      const relativePath = path.join(this.rootDir, dirName)
       const fName = multiMethod
         ? `${fileName}${method}${this.ext}`
-        : `${fileName}${this.ext}`;
-      const filePath = path.join(relativePath, fName);
-      let content = this.getTemplateContent(doc);
+        : `${fileName}${this.ext}`
+      const filePath = path.join(relativePath, fName)
+      let content = this.getTemplateContent(doc)
       content = prettier.format(content, {
         semi: false,
         singleQuote: true,
         printWidth: 130,
-        parser: "babel",
-      });
+        parser: this.ext === '.ts' ? 'typescript' : 'babel',
+      })
       mkdirp(relativePath).then(() => {
-        writeFile(filePath, content);
-      });
+        writeFile(filePath, content)
+      })
     }
   }
   getTypeParams() {
-    const { params } = this.currentDoc;
+    const { params } = this.currentDoc
     if (!params || !params.length === 0) {
-      return;
+      return
     }
-    let bodyParams = "any";
-    const queryParams = [];
+    let bodyParams = 'any'
+    const queryParams = []
     for (let param of params) {
-      if (param.in === "query") {
+      if (param.in === 'query') {
         queryParams.push({
           ...param,
           type: TypeMap[param.type],
-        });
+        })
       }
-      if (param.in === "body") {
-        bodyParams = `defs.${param.schema.originalRef}`;
+      if (param.in === 'body') {
+        bodyParams = `defs.${param.schema.originalRef}`
       }
     }
-    return [queryParams, bodyParams];
+    return [queryParams, bodyParams]
   }
   getTypeQueryParams(paramsList) {
     if (!paramsList || paramsList.length === 0) {
-      return "";
+      return ''
     }
     return paramsList
       .map((param) => {
-        const { description, name, required, type } = param;
+        const { description, name, required, type } = param
         return `
         /** ${description} */
-        ${name}${required ? "" : "?"}: ${type}\n
-      `;
+        ${name}${required ? '' : '?'}: ${type}\n
+      `
       })
-      .join("");
+      .join('')
   }
   getTemplateContent(doc) {
-    console.warn("使用预定义的`getTemplateContent`将无法输出结果");
-    return "";
+    console.warn('使用预定义的`getTemplateContent`将无法输出结果')
+    return ''
   }
 }
 
-module.exports = Template;
+module.exports = Template
